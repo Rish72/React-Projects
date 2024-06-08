0b14f014e194cc507d0c4370713a214e54556370
Rishabh <rishabhgoel2020@gmail.com>
2024-06-08 18:18:28 +0530

    Complete Background Component

---
+++ Project 1 -- DOCS/src/Components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <nav className="absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-2xl">
+        Documents
+      </nav>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[15vw] tracking-tighter leading-none text-zinc-900">
+        Docs
+      </h1>
+    </>
+  );
+}
+
+export default Background;
