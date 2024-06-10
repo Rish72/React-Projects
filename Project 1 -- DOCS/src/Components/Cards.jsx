e3703a3d929e95529ff8e2b35391362df420e2cb
Rishabh <rishabhgoel2020@gmail.com>
2024-06-10 20:51:11 +0530

    PROJECT COMPLETED

--- Project 1 -- DOCS/src/Components/Cards.jsx
+++ Project 1 -- DOCS/src/Components/Cards.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { CiFileOn } from "react-icons/ci";
+import { IoCloseSharp } from "react-icons/io5";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { motion } from "framer-motion"
 
-function Cards() {
+function Cards({data, reference}) {
   return (
-    <div className="relative w-52 h-72 rounded-[50px] bg-zinc-900/90 text-white p-5 overflow-hidden">
+    <motion.div drag dragConstraints={reference} 
+    whileTap={{ scale: 1.125 }} dragElastic={0.7} className="shrink-0 relative w-52 h-72 rounded-[50px] bg-zinc-900/90 text-white p-5 overflow-hidden">
       <CiFileOn />
       <p className="text-sm mt-10 leading-tight">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-        nesciunt ab accusantium.
+        {data.desc}
       </p>
       <div className="footer absolute bottom-0 left-0 w-full">
         <div className="flex pyitem-center justify-between mb-3 py-1 px-8">
-          <h5>0.4MB</h5>
+          <h5>{data.filesize}</h5>
           <span className="w-8 h-8 bg-zinc-700 rounded-full flex items-center justify-center">
-            <MdOutlineFileDownload
+
+            { data.close ?<IoCloseSharp/> :  <MdOutlineFileDownload
               size="0.8rem"
               color="
             #fff"
-            />
+            />}
+            
           </span>
         </div>
-        <div className="tag w-full bg-green-700 py-5 flex items-center justify-center">
-          <h3 className="text-sm">Download Now</h3>
-        </div>
+       {
+        data.tag.isOpen &&  <div className={`tag w-full ${data.tag.tagColor === "blue" ?"bg-blue-600" : "bg-green-600"} py-5 flex items-center justify-center`}>
+        <h3 className="text-sm">{data.tag.tagTitle}</h3>
       </div>
-    </div>
+       }
+      </div>
+    </motion.div>
   );
 }
 
--- Project 1 -- DOCS/src/Components/Foreground.jsx
+++ Project 1 -- DOCS/src/Components/Foreground.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Cards from './Cards';
 
 function Foreground() {
+
+  const ForegroundRef = useRef(null);
+
+  const data = [
+    {
+      desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum!",
+      filesize : "1mb",
+      close: false,
+      tag : { isOpen : true, tagTitle :"Upload Now", tagColor: "blue"}
+    },
+    {
+      desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum!",
+      filesize : "1mb",
+      close: true,
+      tag : { isOpen : true, tagTitle :"Download Now", tagColor: "green"}
+    },
+    {
+      desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum!",
+      filesize : "1mb",
+      close: false,
+      tag : { isOpen : false, tagTitle :"Download Now", tagColor: "green"}
+    },
+    {
+      desc : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum!",
+      filesize : "1mb",
+      close: false,
+      tag : { isOpen : true, tagTitle :"Download Now", tagColor: "green"}
+    }
+  ]
+
   return (
-    <div className='fixed z-[3] top-0 left-0 w-full h-full'>
-        <Cards />
+    <div ref={ForegroundRef} className='fixed z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap p-5'>
+      {
+        data.map((item , ind) => (
+          <Cards data={item} reference={ForegroundRef}/>
+        ))
+      }
     </div>
   )
 }
