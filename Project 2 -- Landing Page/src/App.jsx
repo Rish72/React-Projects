2113b9f7ff77b0dd900d3494acd53dce59081ec2
Rishabh <rishabhgoel2020@gmail.com>
2024-06-13 00:26:44 +0530

    Create Component and add Nav

--- Project 2 -- Landing Page/src/App.jsx
+++ Project 2 -- Landing Page/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import "./App.css"
+import Nav from './Components/Nav'
 function App() {
   return (
-    <div>App</div>
+   <Nav />
   )
 }
 
---
+++ Project 2 -- Landing Page/src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <img src="./public/images/brand_logo.png" alt="" />
+      <ul>
+        <li>MENU</li>
+        <li>LOCATION</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+}
+
+export default Nav;
